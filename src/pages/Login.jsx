2b006827fb0d21c03fb094216ce5af64fcc5dca6
Shashi Bhushan <shashibhushan847305@gmail.com>
2026-01-10 03:26:59 +0530

    Login component ui written

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,34 @@
- function Login() {
-  return <h2>Login Page</h2>;
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div style={{ maxWidth: 400, margin: "100px auto", textAlign: "center" }}>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        style={{ width: "100%", padding: 10, marginBottom: 10 }}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        style={{ width: "100%", padding: 10, marginBottom: 20 }}
+      />
+
+      <button style={{ width: "100%", padding: 10 }}>
+        Login
+      </button>
+    </div>
+  );
 }
+
 export default Login;
